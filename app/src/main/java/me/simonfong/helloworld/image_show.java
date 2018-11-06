77ace7493bd1560def65487b53444e836ad970cd
Simon <simonfong6@gmail.com>
2018-11-05 21:14:00 -0800

    Working simple http requests'

--- app/src/main/java/me/simonfong/helloworld/image_show.java
+++ app/src/main/java/me/simonfong/helloworld/image_show.java
@@ -1,5 +1,6 @@
 package me.simonfong.helloworld;
 
+import android.app.DownloadManager;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
@@ -10,12 +11,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import static com.android.volley.Request.*;
 
 public class image_show extends AppCompatActivity {
     Button button;
     ImageView image;
-
+    RequestQueue queue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +45,38 @@ public class image_show extends AppCompatActivity {
             }
         });
         addListenerOnButton();
+
+
+        // Instantiate the RequestQueue.
+        queue = Volley.newRequestQueue(this);
+    }
+
+    /** Called when the user taps the Request button */
+    public void sendRequest(View view) {
+        // Do something in response to button
+        EditText editText = (EditText) findViewById(R.id.http_url);
+        String url = editText.getText().toString();
+
+        final TextView mTextView = (TextView) findViewById(R.id.http_response);
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        mTextView.setText("Response is: "+ response.substring(0,500));
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                mTextView.setText(String.format("That didn't work!%s", error));
+            }
+        });
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+
     }
 
     public void addListenerOnButton() {
@@ -92,4 +136,6 @@ public class image_show extends AppCompatActivity {
             image.setImageBitmap(imageBitmap);
         }
     }
+
+
 }
